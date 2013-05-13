e423a24f242b23f6e83a64796ac755ea2ec2c9e6
Toby Inkster <mail@tobyinkster.co.uk>
2013-05-13 16:47:46 +0100

    bump version; changelog
    --HG--
    branch : HTML-HTML5-Sanity

--- lib/HTML/HTML5/Sanity.pm
+++ lib/HTML/HTML5/Sanity.pm
@@ -3,11 +3,10 @@ package HTML::HTML5::Sanity;
 use 5.010;
 use strict;
 use warnings;
-use Object::AUTHORITY;
 
 BEGIN {
 	$HTML::HTML5::Sanity::AUTHORITY = 'cpan:TOBYINK';
-	$HTML::HTML5::Sanity::VERSION   = '0.103';
+	$HTML::HTML5::Sanity::VERSION   = '0.104';
 }
 
 require Exporter;
@@ -693,9 +692,15 @@ Toby Inkster E<lt>tobyink@cpan.orgE<gt>.
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2009-2011 by Toby Inkster
+Copyright (C) 2009-2013 by Toby Inkster
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
+
+=head1 DISCLAIMER OF WARRANTIES
+
+THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
 
 =cut
